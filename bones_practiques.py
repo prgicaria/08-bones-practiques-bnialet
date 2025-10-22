7a73bc3fcc3502f0bf9c4e8251f4a4bfc3fd3be7
bnialet <bnialet@instituticaria.cat>
2025-10-22 10:41:35 +0200

    fet

---
+++ bones_practiques.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+'''divisio basica. calcula una division amb coeficient i residu
+Institut Icària
+programació - 1r Batxillerat - curs 2025-2026
+
+_author_=Bruno Nialet
+_email_=Bnialet@instituticaria.cat
+_date_ =2025/10/20
+
+
+
+divident = float(input("introduex el divident"))
+divisor = float(input("introdueix el divisor"))
+
+residu = divident % divisor
+resultat = divident // divisor
+print("resultat final es",resultat)
+print("residu final",residu)'''
+
